7ec461dec9ab608f68361383af56ea6425cbaa6a
Julian Pinzon <julianchos1998@gmail.com>
2022-03-08 11:31:25 -0500

    change plugins to typeScript

---
+++ assets/plugins/AutoPlay.ts
@@ -0,0 +1,16 @@
+import MediaPlayer from "../mediaPlayer";
+
+class AutoPlay {
+    constructor() {
+    }
+    run(player:MediaPlayer) {
+        if (!player.media.muted) {
+            player.media.muted = true;
+
+        }
+        player.play();
+    }
+}
+
+
+export default AutoPlay;
